fd60572b369f3b420f300fd9c5cfb6c42b9f990f
XChanitoX <c.salazar@aprendercreando.com.pe>
2023-06-28 03:58:37 -0500

    Agregando y creando GoRouter

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -47,7 +49,7 @@ class _CustomListTile extends StatelessWidget {
         leading: Icon(menuItem.icon, color: colors.primary),
         trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
         onTap: () {
-          //TODO: Navigate to menuItem.link
+          context.push(menuItem.link);
         });
   }
 }
